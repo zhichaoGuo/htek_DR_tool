bc496503a882ffbfb4a3f0925aa68be3db6cb83a
yaki.guo <yaki.guo@htek.com>
2022-03-02 17:04:41 +0800

    change ui as source

--- main.py
+++ main.py
@@ -1,16 +1,21 @@
-# 这是一个示例 Python 脚本。
+from PySide2.QtWidgets import QApplication
 
-# 按 Shift+F10 执行或将其替换为您的代码。
-# 按 双击 Shift 在所有地方搜索类、文件、工具窗口、操作和设置。
+from ui_main import Ui_TabWidget
 
+class MainWindow(Ui_TabWidget):
 
-def print_hi(name):
-    # 在下面的代码行中使用断点来调试脚本。
-    print(f'Hi, {name}')  # 按 Ctrl+F8 切换断点。
+    def __init__(self):
+        super().__init__()
+        self.ui = Ui_TabWidget()
+        self.ui.Btn_autotest.clicked.connect(self.Btn_autotest)
 
-
+    def f_btn_autotest(self):
+        pass
 # 按间距中的绿色按钮以运行脚本。
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    app = QApplication()
+    gui = MainWindow()
+    app.ui.show(gui)
+    app.exec_()
 
 # 访问 https://www.jetbrains.com/help/pycharm/ 获取 PyCharm 帮助
